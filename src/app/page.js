77a949fb49b394964ac09d945ac0f6e5f89a218c
fahim <fahimahamedweb@gmail.com>
2024-02-15 03:33:58 +0600

    search and swiper done

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@
 import { Fragment, useEffect } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import SearchCom from "../components/SearchCom";
 import Header from "../components/Header";
+import SliderHome from "../components/SliderHome";
 export default function Home({ children,headerExtaClass }) {
   return (
     <Fragment>
-
+    <SearchCom />
     <Header extraClass={headerExtaClass} />
+    <SliderHome />
     {children}
  
   </Fragment>
