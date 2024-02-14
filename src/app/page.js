41aea5d2861fe8685f523436b9b8709b4af49358
fahim <fahimahamedweb@gmail.com>
2024-02-15 03:19:09 +0600

    header created with mui

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { Fragment, useEffect } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import Header from "./Header";
+import Header from "../components/Header";
 export default function Home({ children,headerExtaClass }) {
   return (
     <Fragment>
